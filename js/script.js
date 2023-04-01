0eaa24aae188f00a85b1eeb66f84031f07275d17
Ntsikelelo Sonjica <n.sonjica@gmail.com>
2023-04-01 19:38:23 +0200

    contact form

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const h2tag = document.querySelector("#h2tag");
 const navMenu2 = document.querySelector("#nav-menu");
-const footerTag = document.querySelector("footer");
+const footerTag = document.querySelector(".footer-section");
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+
+    
 
     if(navMenu2.className == "nav-menu active")
     {
